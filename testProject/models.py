78cfbbe00d7ae9acad4a55e64f5697612ce74600
ALittleMoron <dima.lunev14@gmail.com>
2021-04-19 19:08:29 +0300

    дополнение к моделям: теги и категории. Добавлен поиск по новостям

--- testProject/models.py
+++ testProject/models.py
@@ -5,17 +5,23 @@ from datetime import datetime
 from slugify import slugify
 
 
+article_tabs = db.Table(
+    'article_tags',
+    db.Column('article_id', db.Integer, db.ForeignKey('articles.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tags.id'))
+)
+
+
+
 class Article(db.Model):
+    __tablename__ = 'articles'
     id = db.Column(db.Integer, primary_key=True)
     slug = db.Column(db.String(140), unique=True)
     title = db.Column(db.String(140), nullable=False)
     synopsis = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
-
-    def generate_slug(self):
-        if self.title:
-            self.slug = slugify(self.title)
+    category_id = db.Column(db.Integer, db.ForeignKey("categories.id"))
 
     def __init__(self, *args, **kwargs):
         super(Article, self).__init__(*args, **kwargs)
@@ -23,3 +29,51 @@ class Article(db.Model):
 
     def __repr__(self):
         return f'<Article id: {self.id}, title: {self.title}>'
+
+    def generate_slug(self):
+        if self.title:
+            self.slug = slugify(self.title)
+
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    slug = db.Column(db.String(140), unique=True)
+    articles = db.relationship(
+        'Article', 
+        secondary=article_tabs, 
+        backref='tags')
+
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.generate_slug()
+
+    def __repr__(self):
+        return f'<Tag id: {self.id}, name: {self.name}'
+
+    def generate_slug(self):
+        if self.name:
+            self.slug = slugify(self.name)
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    slug = db.Column(db.String(140), unique=True)
+    posts = db.relationship(
+        'Article', 
+        backref='category', 
+        cascade='all,delete-orphan')
+
+    def __init__(self, *args, **kwargs):
+        super(Category, self).__init__(*args, **kwargs)
+        self.generate_slug()
+
+    def __repr__(self):
+        return f'<Category id: {self.id}, name: {self.name}'
+
+    def generate_slug(self):
+        if self.name:
+            self.slug = slugify(self.name)
--- testProject/news/blueprint.py
+++ testProject/news/blueprint.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
-from models import Article
+from models import Article, Tag, Category
 
 news = Blueprint('news', __name__, template_folder='templates',
                  static_folder='static')
@@ -8,14 +8,27 @@ news = Blueprint('news', __name__, template_folder='templates',
 
 @news.route('/')
 def news_page():
-    news = Article.query.all()
+    q = request.args.get('q')
+    print(q)
+    if q:
+        news = Article.query.filter(Article.title.contains(q) | Article.text.contains(q)).all()
+    else:
+        news = Article.query.all()
     return render_template('news/news.html', news=news)
 
 
 @news.route('/<slug>')
 def detail_article(slug):
-    article = Article.query.filter(Article.slug==slug)
-    return render_template('news/detail_article.html', article=article)
+    article = Article.query.filter(Article.slug==slug).first()
+    tags = article.tags
+    return render_template('news/detail_article.html', article=article, tags=tags)
+
+
+@news.route('/tag/<slug>')
+def news_by_tag(slug):
+    tag = Tag.query.filter(Tag.slug==slug).first()
+    news = tag.articles
+    return render_template('news/news_by_tag.html', tag=tag, news=news)
 
 
 @news.route('/add-article')
