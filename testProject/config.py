119d33da3a0c4d95aa43bf5b70b735392b022744
ALittleMoron <dima.lunev14@gmail.com>
2021-04-20 20:52:27 +0300

    админ-панель с можелями

--- testProject/config.py
+++ testProject/config.py
@@ -5,3 +5,4 @@ class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SECRET_KEY = 'a;lkgsa;lksa;lg jsa;j sa;gs;l;sagks;k;'
